0eaeef27541a614385f92546dc45e9a3599c4741
thanos181818 <sahil.mehta23@spit.ac.in>
2026-01-22 11:12:58 +0530

    changes made

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
 import React from 'react';
 import './App.css';
-import illustrationPng from './image.png';
 
 function App() {
   return (
-    <section className="coming-soon" aria-label="Coming Soon">
-      <div className="coming-soon__inner">
-        <div className="coming-soon__illustration" aria-hidden="true">
-          <img className="illustration" src={illustrationPng} alt="" />
-        </div>
-
-        <div className="coming-soon__text">
-          <h1 className="coming-soon__headline">COMING SOON</h1>
-          <div className="coming-soon__subhead" aria-label="Stay tuned">
-            STAY TUNED
-          </div>
-        </div>
+    <div className="app-container">
+      <div className="content">
+        <h1 className="brand-name">BlueberryFin Capital</h1>
+        <h2 className="coming-soon">Coming Soon</h2>
       </div>
-    </section>
+    </div>
   );
 }
 
